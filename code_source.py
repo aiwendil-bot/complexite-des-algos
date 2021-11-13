bd3261f03ba17c1fe9a29d0382cbe94565291fa6
adrien <adriencallico@hotmail.fr>
2021-11-13 19:49:41 +0100

    correction bug (index out of range) affichage résultats instance type I_p

--- code_source.py
+++ code_source.py
@@ -5,7 +5,7 @@ def generer_Ip(p: int):
     D = [1 for i in range(4*p)]
     D += [2 for i in range(2*p*(p-1))]
     D += [2*p]
-    return M,D
+    return M, D
 
 def generer_Ir(m: int, n: int, k: int, dmin: int,dmax: int):
     instances = []
@@ -16,7 +16,8 @@ def generer_Ir(m: int, n: int, k: int, dmin: int,dmax: int):
     return instances
 
 
-def LSA(M: list, D: list):
+def lsa(m: list, D: list):
+    M = m.copy()
     nb_machines = len(M)
     available = [0 for i in range(nb_machines)]
     i = 0
@@ -28,12 +29,16 @@ def LSA(M: list, D: list):
                 chosen_machine = j
                 M[chosen_machine] += D[i]
                 available[chosen_machine] = time + D[i]
-                #print(j, D[i])
+                # print(j, D[i])
                 i += 1
+            if i >= len(D) :
+                break
         time += 1
     return M
 
-def LPT(M: list, D: list):
+def lpt(m: list, d: list):
+    M = m.copy()
+    D = d.copy()
     nb_machines = len(M)
     available = [0 for i in range(nb_machines)]
     i = 0
@@ -48,12 +53,34 @@ def LPT(M: list, D: list):
                 available[chosen_machine] = time + D[i]
                 #print(j, D[i])
                 i += 1
+            if i >= len(D) :
+                break
         time += 1
     return M
 
-def RMA(M: list, D: list):
+def rma(m: list, D: list):
+    M = m.copy()
     nb_machines = len(M)
     for i in range(len(D)):
         chosen_machine = random.randrange(nb_machines)
         M[chosen_machine] += D[i]
     return M
+
+def resultats_Ip(M: list, D: list):
+    res_lsa, res_lpt, res_rma = lsa(M, D), lpt(M, D), rma(M, D)
+    print("Borne inférieure ‘‘maximum’’ = ", max(D))
+    print("Borne inférieure ‘‘moyenne’’ = ", sum(D)/len(D))
+    print("Résultat LSA = ", max(res_lsa))
+    b = max([max(D), sum(D)/len(D)])
+    ratio_lsa = max(res_lsa) / b
+    print("ratio LSA = ", ratio_lsa)
+    print("Résultat LPT = ", max(res_lpt))
+    ratio_lpt = max(res_lpt) / b
+    print("ratio LPT = ", ratio_lpt)
+    print("Résultat RMA = ", max(res_rma))
+    ratio_rma = max(res_rma) / b
+    print("ratio RMA = ", ratio_rma)
+
+
+machines, taches = generer_Ip(10)
+resultats_Ip(machines, taches)
