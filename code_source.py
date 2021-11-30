92bfcbe0fdcf6eec560671a1447d8ec6c539bf24
Nicolas Compère <75181320+ncompere@users.noreply.github.com>
2021-11-30 16:40:19 +0100

    Final (?)

--- code_source.py
+++ code_source.py
@@ -1,5 +1,6 @@
 import random
 
+
 def generer_Ip(p: int):
     M = [0 for i in range(2*p)]
     D = [1 for i in range(4*p)]
@@ -7,12 +8,13 @@ def generer_Ip(p: int):
     D += [2*p]
     return M, D
 
-def generer_Ir(m: int, n: int, k: int, dmin: int,dmax: int):
+
+def generer_Ir(m: int, n: int, k: int, dmin: int, dmax: int):
     instances = []
     for i in range(k):
         M = [0 for i in range(m)]
-        D = [random.randint(dmin,dmax) for i in range(n)]
-        instances.append((M,D))
+        D = [random.randint(dmin, dmax) for i in range(n)]
+        instances.append((M, D))
     return instances
 
 
@@ -30,10 +32,11 @@ def lsa(m: list, D: list):
                 available[chosen_machine] = time + D[i]
                 # print(j, D[i])
                 i += 1
-            if i >= len(D) :
+            if i >= len(D):
                 break
         time += 1
     return M
+
 
 def lpt(m: list, d: list):
     M = m.copy()
@@ -42,7 +45,7 @@ def lpt(m: list, d: list):
     available = [0 for i in range(nb_machines)]
     i = 0
     time = 0
-    D = sorted(D,reverse=True)
+    D = sorted(D, reverse=True)
     while i < len(D):
         for j in range(nb_machines):
             if available[j] == time:
@@ -51,10 +54,11 @@ def lpt(m: list, d: list):
                 available[chosen_machine] = time + D[i]
                 #print(j, D[i])
                 i += 1
-            if i >= len(D) :
+            if i >= len(D):
                 break
         time += 1
     return M
+
 
 def rma(m: list, D: list):
     M = m.copy()
@@ -63,41 +67,3 @@ def rma(m: list, D: list):
         chosen_machine = random.randrange(nb_machines)
         M[chosen_machine] += D[i]
     return M
-
-def resultats_Ip(M: list, D: list):
-    res_lsa, res_lpt, res_rma = lsa(M, D), lpt(M, D), rma(M, D)
-    print("Borne inférieure ‘‘maximum’’ = ", max(D))
-    print("Borne inférieure ‘‘moyenne’’ = ", sum(D)/len(M))
-    print("Résultat LSA = ", max(res_lsa))
-    b = max([max(D), sum(D)/len(M)])
-    ratio_lsa = max(res_lsa) / b
-    print("ratio LSA = ", ratio_lsa)
-    print("Résultat LPT = ", max(res_lpt))
-    ratio_lpt = max(res_lpt) / b
-    print("ratio LPT = ", ratio_lpt)
-    print("Résultat RMA = ", max(res_rma))
-    ratio_rma = max(res_rma) / b
-    print("ratio RMA = ", ratio_rma)
-
-def resultats_Ir(instances: list):
-    somme_lsa, somme_lpt, somme_rma = 0, 0, 0
-    for i in range(len(instances)):
-        m, d = instances[i]
-        res_lsa, res_lpt, res_rma = lsa(m, d), lpt(m, d), rma(m, d)
-        b = max([max(d), sum(d)/len(m)])
-        somme_lsa += max(res_lsa) / b
-        somme_lpt += max(res_lpt) / b
-        somme_rma += max(res_rma) / b
-    print("ratio moyen LSA = ", somme_lsa/len(instances))
-    print("ratio moyen LPT = ", somme_lpt/len(instances))
-    print("ratio moyen RMA = ", somme_rma/len(instances))
-
-
-valeurs = [1,2,3,4,5,6,7,8,9,10,15,20,25,30,35,40,45,50,55,60,70,80,90,100,120,140,160,180,200,300]
-
-for p in valeurs:
-    m,d = generer_Ip(p)
-    res_lpt = lpt(m,d)
-    b = max([max(d), sum(d)/len(m)])
-    ratio_lpt= max(lpt(m, d)) / b
-    print("p = ", p, " , ratio LPT = ", ratio_lpt)
--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import tkinter.messagebox
-
+#!/usr/bin/env python3
 import code_source
+
 from tkinter import *
-from tkinter.messagebox import *
-import tkinter.filedialog
+import tkinter.messagebox
 
 
 class minmakespan(Tk):
@@ -17,9 +16,9 @@ class minmakespan(Tk):
         x = (ws / 2) - (w / 2)
         y = (hs / 2) - (h / 2)
         self.geometry('%dx%d+%d+%d' % (w, h, x, y))
-        self.titre = Label(self, text = "Choisissez le type d'instance :")
-        self.rb1 = Button(self, text = "Instance Ip", command= self.display_Ip)
-        self.rb2 = Button(self, text = "Instance Ir", command = self.display_Ir)
+        self.titre = Label(self, text="Choisissez le type d'instance :")
+        self.rb1 = Button(self, text="Instance Ip", command=self.display_Ip)
+        self.rb2 = Button(self, text="Instance Ir", command=self.display_Ir)
         self.titre.pack()
         self.rb1.pack()
         self.rb2.pack()
@@ -27,9 +26,11 @@ class minmakespan(Tk):
     def display_Ip(self):
         window2 = instance_Ip()
         window2.mainloop()
+
     def display_Ir(self):
         window3 = instance_Ir()
         window3.mainloop()
+
 
 class instance_Ip(Tk):
     def __init__(self):
@@ -42,33 +43,36 @@ class instance_Ip(Tk):
         x = (ws / 2) - (w / 2)
         y = (hs / 2) - (h / 2)
         self.geometry('%dx%d+%d+%d' % (w, h, x, y))
-        self.label = Label(self, text = "Choisissez un entier p :")
+        self.label = Label(self, text="Choisissez un entier p :")
         self.label.pack()
-        self.entree = Entry(self, textvariable = int, width=30)
+        self.entree = Entry(self, textvariable=int, width=30)
         self.entree.pack()
-        self.submit = Button(self, text = "Valider", command = self.display_Ip)
+        self.submit = Button(self, text="Valider", command=self.display_Ip)
         self.submit.pack()
 
     def display_Ip(self):
         try:
             pp = int(self.entree.get())
             M, D = code_source.generer_Ip(pp)
-            res_lsa, res_lpt, res_rma = code_source.lsa(M, D), code_source.lpt(M, D), code_source.rma(M, D)
+            res_lsa, res_lpt, res_rma = code_source.lsa(
+                M, D), code_source.lpt(M, D), code_source.rma(M, D)
             b = max([max(D), sum(D)/len(D)])
             ratio_lsa = max(res_lsa) / b
             ratio_lpt = max(res_lpt) / b
             ratio_rma = max(res_rma) / b
 
-            texte = "Borne inférieure ‘‘maximum’’ = " + str(max(D)) + "\n" + "Borne inférieure ‘‘moyenne’’ = " + \
-                    str(sum(D)/len(D)) + "\n" + "Résultat LSA = " + str(max(res_lsa)) + \
-                    "\n" + "ratio LSA = " + str(ratio_lsa) + "\n" + "Résultat LPT = " + str(max(res_lpt)) + "\n" + \
-                    "ratio LPT = " + str(ratio_lpt) + "\n" + "Résultat RMA = " + str(max(res_rma)) + "\n" + \
+            texte = "Borne inferieure \"maximum\" = " + str(max(D)) + "\n" + "Borne inferieure \"moyenne\" = " + \
+                    str(sum(D)/len(D)) + "\n" + "Resultat LSA = " + str(max(res_lsa)) + \
+                    "\n" + "ratio LSA = " + str(ratio_lsa) + "\n" + "Resultat LPT = " + str(max(res_lpt)) + "\n" + \
+                    "ratio LPT = " + str(ratio_lpt) + "\n" + "Resultat RMA = " + str(max(res_rma)) + "\n" + \
                     "ratio RMA = " + str(ratio_rma)
-            tkinter.messagebox.showinfo( "Résultats", texte)
+            tkinter.messagebox.showinfo("Resultats", texte)
             self.destroy()
         except ValueError:
-            tkinter.messagebox.showerror( "Erreur", "Vous devez saisir un entier !" )
+            tkinter.messagebox.showerror(
+                "Erreur", "Vous devez saisir un entier !")
             self.destroy()
+
 
 class instance_Ir(Tk):
     def __init__(self):
@@ -81,17 +85,20 @@ class instance_Ir(Tk):
         x = (ws / 2) - (w / 2)
         y = (hs / 2) - (h / 2)
         self.geometry('%dx%d+%d+%d' % (w, h, x, y))
-        self.label1 = Label(self, text = "Choisissez un nombre de machines :")
+        self.label1 = Label(self, text="Choisissez un nombre de machines :")
         self.entree1 = Entry(self, width=30)
-        self.label2 = Label(self, text = "Choisissez un nombre de tâches :")
+        self.label2 = Label(self, text="Choisissez un nombre de taches :")
         self.entree2 = Entry(self, width=30)
-        self.label3 = Label(self, text = "Choisissez un nombre d'instances à générer :")
+        self.label3 = Label(
+            self, text="Choisissez un nombre d'instances a generer :")
         self.entree3 = Entry(self, width=30)
-        self.label4 = Label(self, text = "Choisissez une durée de tâche minimum :")
+        self.label4 = Label(
+            self, text="Choisissez une duree de tache minimum :")
         self.entree4 = Entry(self, width=30)
-        self.label5 = Label(self, text = "Choisissez une durée de tâche maximum :")
+        self.label5 = Label(
+            self, text="Choisissez une duree de tache maximum :")
         self.entree5 = Entry(self, width=30)
-        self.submit = Button(self, text = "Valider", command = self.display_Ir)
+        self.submit = Button(self, text="Valider", command=self.display_Ir)
         self.label1.pack()
         self.entree1.pack()
         self.label2.pack()
@@ -115,18 +122,22 @@ class instance_Ir(Tk):
             somme_lsa, somme_lpt, somme_rma = 0, 0, 0
             for i in range(len(instances)):
                 m, d = instances[i]
-                res_lsa, res_lpt, res_rma = code_source.lsa(m, d), code_source.lpt(m, d), code_source.rma(m, d)
+                res_lsa, res_lpt, res_rma = code_source.lsa(
+                    m, d), code_source.lpt(m, d), code_source.rma(m, d)
                 b = max([max(d), sum(d)/len(d)])
                 somme_lsa += max(res_lsa) / b
                 somme_lpt += max(res_lpt) / b
                 somme_rma += max(res_rma) / b
 
             texte = "ratio moyen LSA = " + str(somme_lsa/len(instances)) + "\n" + "ratio moyen LPT = " + \
-                    str(somme_lpt/len(instances)) + "\n" +"ratio moyen RMA = " + str(somme_rma/len(instances))
-            tkinter.messagebox.showinfo( "Résultats", texte)
+                    str(somme_lpt/len(instances)) + "\n" + \
+                "ratio moyen RMA = " + str(somme_rma/len(instances))
+            tkinter.messagebox.showinfo("Resultats", texte)
             self.destroy()
         except ValueError:
-            tkinter.messagebox.showerror( "Erreur", "Vous devez saisir un (des) entier(s) !" )
+            tkinter.messagebox.showerror(
+                "Erreur", "Vous devez saisir un (des) entier(s) !")
+
 
 window = minmakespan()
 window.mainloop()
